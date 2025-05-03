0ea02c2cb4e5da7279094afaf18fcfeab99ca42d
ArowuTest <fsanus20111@gmail.com>
2025-05-03 22:48:43 +0100

    Delete internal/models/prize.go

--- internal/models/prize.go
+++
@@ -1,8 +0,0 @@
-package models
-
-// Prize defines the structure for a single prize category within a draw
-type Prize struct {
-	Category   string  `bson:"category" json:"category"`     // e.g., "JACKPOT", "2ND_PRIZE", "CONSOLATION"
-	Amount     float64 `bson:"amount" json:"amount"`         // Prize amount for this category
-	NumWinners int     `bson:"numWinners" json:"numWinners"` // Number of winners for this category
-}
