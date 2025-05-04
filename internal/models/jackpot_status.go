7e72d3e5771abb8d1651996d8d1ad4f47955de3a
ArowuTest <fsanus20111@gmail.com>
2025-05-04 06:44:29 +0100

    Update jackpot_status.go

--- internal/models/jackpot_status.go
+++ internal/models/jackpot_status.go
@@ -8,9 +8,10 @@ import (
 type JackpotStatus struct {
 	CurrentAmount    float64   `json:"currentAmount"`
 	LastDrawDate     time.Time `json:"lastDrawDate"` // Date of the last relevant draw
-	LastWinnerMSISDN string    `json:"lastWinnerMsisdn,omitempty"` // MSISDN of the last jackpot winner (if any)
-	LastWinAmount    float64   `json:"lastWinAmount,omitempty"`    // Amount won by the last jackpot winner (if any)
+	LastWinnerMSISDN string    `json:"lastWinnerMsisdn,omitempty"` // Added: MSISDN of the last jackpot winner (if any)
+	LastWinAmount    float64   `json:"lastWinAmount,omitempty"`    // Added: Amount won by the last jackpot winner (if any)
+	NextDrawDate     time.Time `json:"nextDrawDate,omitempty"`    // Added: Date of the next scheduled draw
 	LastUpdatedAt    time.Time `json:"lastUpdatedAt"`
-	// Add other relevant fields like next draw date if needed
 }
+
 
